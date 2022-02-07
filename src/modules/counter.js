fea4b858132028ff5858c628c014055c4693c34c
yunhosung <yhs4157@naver.com>
2022-02-07 21:51:28 +0900

    add counter action and reducer

---
+++ src/modules/counter.js
@@ -0,0 +1,29 @@
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+// 액션 타입 정의
+
+export const increase = () => ({type:INCREASE});
+export const decrease = () => ({type:DECREASE});
+// 액션 생성 함수 만들기 
+
+const initialState = {
+    number: 0
+};
+
+function counter(state = initialState, action) {
+    switch(action.type) {
+        case INCREASE: 
+        return {
+            number: state.number + 1
+        };
+        
+        case DECREASE: 
+        return {
+            number: state.number - 1
+        };
+
+        default: return state; 
+    }
+}
+
+export default counter; 
